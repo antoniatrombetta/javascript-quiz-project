c44a618aff367783627a27102cb3753768858258
antonia trombetta <antonia.dorazio@gmail.com>
2024-07-22 17:13:04 +0200

    almost done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,31 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
     
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
-}
+    }
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
+    shuffleQuestions() {
+        this.questions.sort(function(a, b){return 0.5 - Math.random()});
+    }
+    checkAnswer(answer) {
+       if (answer === this.getQuestion().answer){
+        this.correctAnswers++;
+       }
+    }
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
